d3985ce0ce0e217d24be859a0ade803394af3bcc
David Knörzer <33221434+davidknoerzer@users.noreply.github.com>
2024-02-09 03:35:02 +0100

    fixed layout.tsx linting bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,14 @@ import type { Metadata } from "next";
 import { Rubik, Nanum_Gothic_Coding } from "next/font/google";
 import "./globals.css";
 
-export const font = Rubik({
-  weight: ['500'],
-  subsets: ['latin']
-})
+const font = Rubik({
+  weight: ["500"],
+  subsets: ["latin"],
+});
 export const codingFont = Nanum_Gothic_Coding({
-  weight: ['400'],
-  subsets: ['latin']
-})
+  weight: ["400"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "David's Portfolio",
